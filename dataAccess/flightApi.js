a86cba62f1fc40d39ffd6029d08f93169b33986b
vuruxy <kaunghtetoo07@gmail.com>
2020-12-16 20:35:05 +0800

    Flight api with locales, countries and such

--- dataAccess/flightApi.js
+++ dataAccess/flightApi.js
@@ -7,7 +7,19 @@ module.exports = {
         return retVal;
     },
     getFlightInfo: async () => {
-        //let retVal = await got.default.get(config.ApiUrl + '/geo/v1.0?apikey=' + config.ApiKey);
-        //return retVal;
+        let retVal = await got.default.get(config.ApiUrl + '/v1/flights?apikey=' + config.ApiKey);
+        return retVal;
+    },
+    getCountries: async () => {
+        let retVal = await got.default.get(config.ApiUrl + '/v1/countries?apikey=' + config.ApiKey);
+        return retVal;
+    },
+    getLocales: async () => {
+        let retVal = await got.default.get(config.ApiUrl + '/v1.0/locales?apikey='+ config.ApiKey);
+        return retVal;
+    },
+    getCurrencies: async () =>{
+        let retVal = await got.default.get(config.ApiUrl + '/v1.0/currencies?apikey='+config.ApiKey);
+        return retVal;
     }
 }
--- services/flightService.js
+++ services/flightService.js
@@ -2,6 +2,18 @@ let flightApi = require('../dataAccess/flightApi');
 
 module.exports = {
     getPlaces: async () => {
-        return await flightApi.getPlaces();
+        return await flightApi.getPlaces();   
+    },
+    getCountires: async () => {
+        return await flightApi.getCountries();
+    },
+    getFlightsInfo:async() => {
+        return await flightApi.getFlightInfo();
+    },
+    getLocales: async() => {
+        return await flightApi.getLocales();
+    },
+    getCurrencies: async() => {
+        return await flightApi.getCurrencies();
     }
 }
