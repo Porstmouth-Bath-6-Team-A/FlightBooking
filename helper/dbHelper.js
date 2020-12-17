5bc2c73d36d58118d00c35108e8f6486fdf0ffeb
zanhtet <nyuntzanhtet@gmail.com>
2020-12-17 22:26:58 +0800

    add DBHelper

---
+++ helper/dbHelper.js
@@ -0,0 +1,89 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = "mongodb+srv://admin:admin@cluster0.srmns.mongodb.net/flightBooking?retryWrites=true&w=majority";
+
+module.exports = {
+    insert: async (collectionName, document) => {
+        let client = new MongoClient(url, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        try {
+            await client.connect();
+        
+            let db = await client.db('flightBooking');
+            let col = await db.collection(collectionName);
+
+            await col.insertOne(document);
+         
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    },
+    delete: async (collectionName, query) => {
+        let client = new MongoClient(url, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        try {
+            await client.connect();
+        
+            let db = await client.db('flightBooking');
+            let col = await db.collection(collectionName);
+
+            await col.deleteMany(query);
+         
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    },
+    update: async (collectionName, document, query) => {
+        let client = new MongoClient(url, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        try {
+            await client.connect();
+        
+            let db = await client.db('flightBooking');
+            let col = await db.collection(collectionName);
+
+            await col.updateMany(query, {$set: document}, {multi: true });
+         
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    },
+    find: async (collectionName, query) => {
+        let client = new MongoClient(url, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        try {
+            await client.connect();
+        
+            let db = await client.db('flightBooking');
+            let col = await db.collection(collectionName);
+            let cursor = col.find(query);
+
+            let retVal = [];
+            for (let doc = await cursor.next(); doc; doc = await cursor.next()) {
+                retVal.push(doc);
+            }
+
+            return retVal;
+         
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 const flightService = require("../FlightBooking/services/flightService");
+const dbHelper = require('../FlightBooking/helper/dbHelper');
+
+app.get('/test', async (req, res) => {
+    await dbHelper.insert('country', {Code: "SG", Name: "SINGAPORE"});
+
+    let country = await dbHelper.find('country', {}); 
+    console.log(country);
+    console.log('------------');
+
+    await dbHelper.update('country', {Code: "SG"}, {Code: "MY", Name: "MYANMMAR"});
+
+    country = await dbHelper.find('country', {}); 
+    console.log(country);
+    console.log('------------');
+
+    await dbHelper.delete('country', {Code: "SG"});
+
+    country = await dbHelper.find('country', {}); 
+    console.log(country);
+    console.log('------------');
+
+    res.send('test');
+});
 
 app.get('/home', async (req, res) => {
     let val = await flightService.getPlaces();
