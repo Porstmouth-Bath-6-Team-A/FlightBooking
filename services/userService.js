c9cb201a2e6be083f6254dd41009ea12ba9019b3
vuruxy <kaunghtetoo07@gmail.com>
2021-01-02 12:00:27 +0800

    userData

---
+++ services/userService.js
@@ -0,0 +1,25 @@
+const userData = require("../dataAccess/userData");
+const dbHelper = require("./helper/dbHelper");
+
+
+module.exports ={
+    getUser: async () => {
+        return await userData.getUser(); 
+    },
+    updateUser: async () => {
+    
+    },
+    deleteUser: async () => {
+        return await userData.deleteUser();  
+    },
+    postUser: async () => {
+        return await userData.postUser();
+    },
+    getLogInStatus: async () => {
+    
+    },
+    postLogOut: () => {
+        userData.destroy();
+    }
+}
+
