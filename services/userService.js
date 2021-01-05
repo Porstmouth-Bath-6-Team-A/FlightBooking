5636bc649b2622c90a787d9db17bababe317252a
vuruxy <kaunghtetoo07@gmail.com>
2021-01-05 16:14:42 +0800

    userServices and userData

--- services/userService.js
+++ services/userService.js
@@ -1,25 +1,43 @@
 const userData = require("../dataAccess/userData");
-const dbHelper = require("./helper/dbHelper");
+const dbHelper = require("../helper/dbHelper");
 
 
 module.exports ={
-    getUser: async () => {
-        return await userData.getUser(); 
+    getUser: async (emailAddress,password) => {
+        return await userData.getUser(emailAddress,password); 
     },
-    updateUser: async () => {
-    
+    updateUser: async (firstName,lastName,phoneNumber,address,emailAddress) => {
+		return await userData.updateUser(firstName,lastName,phoneNumber,address,emailAddress);
     },
-    deleteUser: async () => {
-        return await userData.deleteUser();  
+    deleteUser: async (emailAddress) => {
+        return await userData.deleteUser(emailAddress);  
     },
-    postUser: async () => {
-        return await userData.postUser();
+    insertUser: async (firstName,lastName,phoneNumber,address,emailAddress) => {
+        return await userData.insertUser(firstName,lastName,phoneNumber,address,emailAddress);
     },
-    getLogInStatus: async () => {
-    
+    insertlogin: async (emailAddress,token,updatedTokenTime) => {
+        return await userData.insertlogin(emailAddress,token,updatedTokenTime);
     },
-    postLogOut: () => {
-        userData.destroy();
+    isLogin: async (emailAddress,token,currentTime) => {
+        let login = await userData.getLogin(emailAddress,token,currentTime);
+
+        if (login) {
+            let diffMs = login.updatedTokenTime - new Date();
+            let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); 
+
+            if (diffMins > 5) {
+                await userData.insertLogout(emailAddress);
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+
+    },
+    insertLogout: async (emailAddress) => {
+        return await userData.insertLogout(emailAddress);
     }
 }
 
